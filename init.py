dc05defba34da03b3338e7439e819402d588a485
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2022-03-18 01:59:22 +0100

    Add board

--- init.py
+++ init.py
@@ -2,91 +2,101 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-COLORS = ['red', 'blue', 'yellow', 'lime']
+COLORS = ['red', 'blue', 'purple', 'pink']
 
 cred = credentials.Certificate('serviceAccountKey.json')
 
 firebase_admin.initialize_app(cred, {
-  'databaseURL': "https://calzan-default-rtdb.europe-west1.firebasedatabase.app/"
+    'databaseURL': "https://calzan-default-rtdb.europe-west1.firebasedatabase.app/"
 })
 
 players = {}
 for color in COLORS:
-  players[color] = {
-    "Holz": 0,
-    "Lehm": 0,
-    "Stroh": 0,
-    "Schafe": 0,
-    "Erz": 0,
-    "Siegpunkte": 0,
+    players[color] = {
+        'res': {'Holz': 0, 'Lehm': 0, 'Stroh': 0, 'Schafe': 0, 'Erz': 0},
+        'towns': [],
+        'roads': [],
+    }
+
+  # TODO remove
+players['red']['towns'].append({'x': , 'y':, 'city': False})
+
+  (0, 0, 'town', {'color': 'red'}),
+  (0, 0, 'city', {'color': 'blue'}),
+roads = [
+  (0, 0, 'road', {'color': 'purple', }),
+  (0, 0, 'road', {'color': 'pink'}),
+]
+
   }
 
 config = [
-  ("Meer", 0, 0, 3),
-  ("Meer", 0, 0, 5),
-  ("Meer", 0, 0, 7),
-  ("Meer", 0, 0, 9),
+  (0, 0, 'tile', {'res': 'Sand'}),
+  (0, 0, 'bandit', {}),
 
-  ("Meer", 0, 1, 2),
-  ("Stroh", 4, 1, 4),
-  ("Holz", 6, 1, 6),
-  ("Stroh", 9, 1, 8),
-  ("Meer", 0, 1, 10),
+  (1, -1, 'tile', {'res': 'Schafe', 'roll': 12}),
+  (2, 1, 'tile', {'res': 'Erz', 'roll': 8}),
+  (1, 2, 'tile', {'res': 'Lehm', 'roll': 10}),
+  (-1, 1, 'tile', {'res': 'Erz', 'roll': 5}),
+  (-2, -1, 'tile', {'res': 'Lehm', 'roll': 8}),
+  (-1, -2, 'tile', {'res': 'Holz', 'roll': 5}),
 
-  ("Meer", 0, 2, 1),
-  ("Lehm", 2, 2, 3),
-  ("Holz", 5, 2, 5),
-  ("Schafe", 12, 2, 7),
-  ("Schafe", 4, 2, 9),
-  ("Meer", 0, 2, 11),
+  (2, -2, 'tile', {'res': 'Stroh', 'roll': 9}),
+  (3, 0, 'tile', {'res': 'Schafe', 'roll': 4}),
+  (4, 2, 'tile', {'res': 'Schafe', 'roll': 10}),
+  (3, 3, 'tile', {'res': 'Holz', 'roll': 11}),
+  (2, 4, 'tile', {'res': 'Erz', 'roll': 11}),
+  (0, 3, 'tile', {'res': 'Stroh', 'roll': 6}),
+  (-2, 2, 'tile', {'res': 'Stroh', 'roll': 3}),
+  (-3, 0, 'tile', {'res': 'Holz', 'roll': 3}),
+  (-4, -2, 'tile', {'res': 'Schafe', 'roll': 9}),
+  (-3, -3, 'tile', {'res': 'Lehm', 'roll': 2}),
+  (-2, -4, 'tile', {'res': 'Stroh', 'roll': 4}),
+  (0, -3, 'tile', {'res': 'Holz', 'roll': 6}),
 
-  ("Meer", 0, 3, 0),
-  ("Schafe", 9, 3, 2),
-  ("Lehm", 8, 3, 4),
-  ("Sand", 0, 3, 6),
-  ("Erz", 8, 3, 8),
-  ("Schafe", 10, 3, 10),
-  ("Meer", 0, 3, 12),
+  (-2, -5, 'port', {}),
+  (-3, -5, 'port', {}),
+  (-3, -6, 'trade', {'rate': 3}),
 
-  ("Meer", 0, 4, 1),
-  ("Holz", 3, 4, 3),
-  ("Erz", 5, 4, 5),
-  ("Lehm", 10, 4, 7),
-  ("Holz", 11, 4, 9),
-  ("Meer", 0, 4, 11),
+  (0, -4, 'port', {}),
+  (1, -3, 'port', {}),
+  (1, -4, 'trade', {'rate': 2, 'res': 'Schafe'}),
 
-  ("Meer", 0, 5, 2),
-  ("Stroh", 3, 5, 4),
-  ("Stroh", 6, 5, 6),
-  ("Erz", 11, 5, 8),
-  ("Meer", 0, 5, 10),
+  (3, -1, 'port', {}),
+  (4, 0, 'port', {}),
+  (4, -1, 'trade', {'rate': 3}),
 
-  ("Meer", 0, 6, 3),
-  ("Meer", 0, 6, 5),
-  ("Meer", 0, 6, 7),
-  ("Meer", 0, 6, 9),
+  (5, 2, 'port', {}),
+  (5, 3, 'port', {}),
+  (6, 3, 'trade', {'rate': 3}),
+
+  (3, 4, 'port', {}),
+  (4, 4, 'port', {}),
+  (4, 5, 'trade', {'rate': 2, 'res': 'Lehm'}),
+
+  (0, 4, 'port', {}),
+  (1, 4, 'port', {}),
+  (1, 5, 'trade', {'rate': 2, 'res': 'Holz'}),
+
+  (-3, 2, 'port', {}),
+  (-2, 3, 'port', {}),
+  (-3, 3, 'trade', {'rate': 3}),
+
+  (-4, 0, 'port', {}),
+  (-4, -1, 'port', {}),
+  (-5, -1, 'trade', {'rate': 2, 'res': 'Stroh'}),
+
+  (-4, -3, 'port', {}),
+  (-4, -4, 'port', {}),
+  (-5, -4, 'trade', {'rate': 2, 'res': 'Erz'}),
 ]
 
-tiles = []
-
-for land, num, y, x in config:
-    tiles.append({
-        "type": land,
-        "num": num,
-        "x": x,
-        "y": y,
-        "above": {"color": "", "city": False},
-        "below": {"color": "", "city": False},
-        "upright": {"color": ""},
-        "right": {"color": ""},
-        "lowright": {"color": ""},
-        "bandit": land == 'Sand',
-    })
+tokens = [{"x": x, "y": y, "t": t, "a": a} for x, y, t, a in config]
 
 data = {
-    'dice': 0,
-    'tiles': tiles,
     'players': players,
+    'dice': 0,
+    'tokens': tokens,
 }
 
 db.reference('/').set(data)
